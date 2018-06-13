088600a9e89e84841e90ef80781a233125daf480
Ubuntu <ubuntu@ip-172-31-44-55.us-east-2.compute.internal>
2018-06-13 18:34:10 +0000

    saving incase

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../build'));
 
 app.listen(3000, () => console.log('Express is listening on port 3000'))
